ccb47f0169ff467599454b6f67726b9720af66e1
liewvk <voonkiong@gamil.com>
2026-07-14 19:10:56 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace ToDoListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.lblPriority = new System.Windows.Forms.Label();
+            this.cmbPriority = new System.Windows.Forms.ComboBox();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.chkCompleted = new System.Windows.Forms.CheckBox();
+            this.grpTaskCenter = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClearInput = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.grpTaskCenter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(233, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(211, 38);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "To-Do List App";
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTask.Location = new System.Drawing.Point(279, 70);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(62, 25);
+            this.lblTask.TabIndex = 1;
+            this.lblTask.Text = "Task:";
+            // 
+            // txtTask
+            // 
+            this.txtTask.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(350, 70);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(300, 30);
+            this.txtTask.TabIndex = 2;
+            // 
+            // lblPriority
+            // 
+            this.lblPriority.AutoSize = true;
+            this.lblPriority.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPriority.Location = new System.Drawing.Point(264, 131);
+            this.lblPriority.Name = "lblPriority";
+            this.lblPriority.Size = new System.Drawing.Size(77, 25);
+            this.lblPriority.TabIndex = 3;
+            this.lblPriority.Text = "Priority:";
+            this.lblPriority.UseMnemonic = false;
+            // 
+            // cmbPriority
+            // 
+            this.cmbPriority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPriority.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPriority.FormattingEnabled = true;
+            this.cmbPriority.Location = new System.Drawing.Point(350, 131);
+            this.cmbPriority.Name = "cmbPriority";
+            this.cmbPriority.Size = new System.Drawing.Size(290, 33);
+            this.cmbPriority.TabIndex = 4;
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDueDate.Location = new System.Drawing.Point(241, 186);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(100, 25);
+            this.lblDueDate.TabIndex = 5;
+            this.lblDueDate.Text = "Due Date:";
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDueDate.Location = new System.Drawing.Point(370, 181);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(178, 30);
+            this.dtpDueDate.TabIndex = 6;
+            // 
+            // chkCompleted
+            // 
+            this.chkCompleted.AutoSize = true;
+            this.chkCompleted.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCompleted.Location = new System.Drawing.Point(63, 412);
+            this.chkCompleted.Name = "chkCompleted";
+            this.chkCompleted.Size = new System.Drawing.Size(129, 29);
+            this.chkCompleted.TabIndex = 7;
+            this.chkCompleted.Text = "Completed";
+            this.chkCompleted.UseVisualStyleBackColor = true;
+            // 
+            // grpTaskCenter
+            // 
+            this.grpTaskCenter.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.grpTaskCenter.Controls.Add(this.btnExit);
+            this.grpTaskCenter.Controls.Add(this.btnClearAll);
+            this.grpTaskCenter.Controls.Add(this.btnClearInput);
+            this.grpTaskCenter.Controls.Add(this.btnDelete);
+            this.grpTaskCenter.Controls.Add(this.btnUpdate);
+            this.grpTaskCenter.Controls.Add(this.btnAdd);
+            this.grpTaskCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpTaskCenter.Location = new System.Drawing.Point(36, 70);
+            this.grpTaskCenter.Name = "grpTaskCenter";
+            this.grpTaskCenter.Size = new System.Drawing.Size(182, 322);
+            this.grpTaskCenter.TabIndex = 8;
+            this.grpTaskCenter.TabStop = false;
+            this.grpTaskCenter.Text = "Task Center";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(23, 53);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(138, 33);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(23, 100);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(138, 31);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update Task";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(23, 137);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(138, 31);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClearInput
+            // 
+            this.btnClearInput.Location = new System.Drawing.Point(23, 181);
+            this.btnClearInput.Name = "btnClearInput";
+            this.btnClearInput.Size = new System.Drawing.Size(135, 30);
+            this.btnClearInput.TabIndex = 3;
+            this.btnClearInput.Text = "Clear Input";
+            this.btnClearInput.UseVisualStyleBackColor = true;
+            this.btnClearInput.Click += new System.EventHandler(this.btnClearInput_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(23, 228);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(135, 31);
+            this.btnClearAll.TabIndex = 4;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(23, 265);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(133, 32);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 16;
+            this.lstTasks.Location = new System.Drawing.Point(246, 251);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(527, 180);
+            this.lstTasks.TabIndex = 9;
+            this.lstTasks.SelectedIndexChanged += new System.EventHandler(this.lstTasks_SelectedIndexChanged);
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetails.Location = new System.Drawing.Point(241, 449);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(208, 25);
+            this.lblDetails.TabIndex = 10;
+            this.lblDetails.Text = "Selected Task Details:";
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDetails.Location = new System.Drawing.Point(246, 495);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.Size = new System.Drawing.Size(526, 30);
+            this.txtDetails.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 546);
+            this.Controls.Add(this.txtDetails);
+            this.Controls.Add(this.lblDetails);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.grpTaskCenter);
+            this.Controls.Add(this.chkCompleted);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.cmbPriority);
+            this.Controls.Add(this.lblPriority);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To-Do List App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpTaskCenter.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Label lblPriority;
+        private System.Windows.Forms.ComboBox cmbPriority;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.CheckBox chkCompleted;
+        private System.Windows.Forms.GroupBox grpTaskCenter;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClearInput;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.TextBox txtDetails;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListApp
+{
+    public partial class Form1 : Form
+    {
+        private void ClearInputFields()
+        {
+            txtTask.Clear();
+            cmbPriority.SelectedIndex = 1;
+            dtpDueDate.Value = DateTime.Today;
+            chkCompleted.Checked = false;
+            txtDetails.Clear();
+
+            txtTask.Focus();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPriority.Items.Add("Low");
+            cmbPriority.Items.Add("Medium");
+            cmbPriority.Items.Add("High");
+
+            cmbPriority.SelectedIndex = 1;
+
+            dtpDueDate.Value = DateTime.Today;
+            txtTask.Focus();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string task = txtTask.Text.Trim();
+
+            if (task == "")
+            {
+                MessageBox.Show("Please enter a task.",
+                                "Missing Task",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtTask.Focus();
+                return;
+            }
+
+            if (cmbPriority.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a priority.",
+                                "Missing Priority",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                cmbPriority.Focus();
+                return;
+            }
+
+            string priority = cmbPriority.SelectedItem.ToString();
+            string dueDate = dtpDueDate.Value.ToShortDateString();
+            string status = chkCompleted.Checked ? "Completed" : "Pending";
+
+            string taskItem = $"[{status}] {task} | Priority: {priority} | Due: {dueDate}";
+
+            lstTasks.Items.Add(taskItem);
+
+            ClearInputFields();
+
+            MessageBox.Show("Task added successfully.",
+                            "Task Added",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+        }
+
+        private void btnClearInput_Click(object sender, EventArgs e)
+        {
+                        ClearInputFields();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.Items.Count == 0)
+            {
+                MessageBox.Show("There are no tasks to clear.",
+                                "Empty List",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Are you sure you want to clear all tasks?",
+                                                  "Confirm Clear All",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                lstTasks.Items.Clear();
+                ClearInputFields();
+
+                MessageBox.Show("All tasks have been cleared.",
+                                "Tasks Cleared",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (lstTasks.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            string selectedTask = lstTasks.SelectedItem.ToString();
+
+            txtDetails.Text = selectedTask;
+            txtTask.Text = selectedTask;
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a task to update.",
+                                "No Task Selected",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            string task = txtTask.Text.Trim();
+
+            if (task == "")
+            {
+                MessageBox.Show("Please enter a task.",
+                                "Missing Task",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                txtTask.Focus();
+                return;
+            }
+
+            string priority = cmbPriority.SelectedItem.ToString();
+            string dueDate = dtpDueDate.Value.ToShortDateString();
+            string status = chkCompleted.Checked ? "Completed" : "Pending";
+
+            string updatedTask = $"[{status}] {task} | Priority: {priority} | Due: {dueDate}";
+
+            lstTasks.Items[lstTasks.SelectedIndex] = updatedTask;
+
+            txtDetails.Text = updatedTask;
+
+            MessageBox.Show("Task updated successfully.",
+                            "Task Updated",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+        }
+
+        
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?",
+                                      "Confirm Exit",
+                                      MessageBoxButtons.YesNo,
+                                      MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a task to delete.",
+                                "No Task Selected",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Are you sure you want to delete the selected task?",
+                                                  "Confirm Delete",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                ClearInputFields();
+
+                MessageBox.Show("Task deleted successfully.",
+                                "Task Deleted",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
